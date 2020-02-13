75464db0cfc8e76073d8706a66897f9ed8122099
Gavin Taskey <gataskey@bsu.edu>
2020-02-13 11:47:13 -0500

    -Extracting Methods

--- src/InsuranceCalculator.java
+++ src/InsuranceCalculator.java
@@ -2,14 +2,30 @@ public class InsuranceCalculator {
 
     public double calculateInsurance(double income) {
         if (income <= 10000) {
-            return income*0.365;
+            return lowInsuranceCalc(income);
         } else if (income <= 30000) {
-            return (income-10000)*0.2+35600;
+            return medInsuranceCalc(income, 10000, 0.2, 35600);
         } else if (income <= 60000) {
-            return (income-30000)*0.1+76500;
+            return highInsuranceCalc(income, 30000, 0.1, 76500);
         } else {
-            return (income-60000)*0.02+105600;
+            return veryHighInsuranceCalc(income, 60000, 0.02, 105600);
         }
+    }
+
+    private double veryHighInsuranceCalc(double income, int i, double v, int i2) {
+        return (income - i) * v + i2;
+    }
+
+    private double highInsuranceCalc(double income, int i, double v, int i2) {
+        return (income - i) * v + i2;
+    }
+
+    private double medInsuranceCalc(double income, int i, double v, int i2) {
+        return (income - i) * v + i2;
+    }
+
+    private double lowInsuranceCalc(double income) {
+        return income*0.365;
     }
 
 }
