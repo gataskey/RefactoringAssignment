d4fddbc02c2a4ecc3cebf88d62d82609d8d1c620
Gavin Taskey <gataskey@bsu.edu>
2020-02-13 11:45:33 -0500

    - Creating and running tests

--- test/InsuranceCalculatorTest.java
+++ test/InsuranceCalculatorTest.java
@@ -1,7 +1,37 @@
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+public class InsuranceCalculatorTest {
 
-class InsuranceCalculatorTest {
+    @Test
+    public void lowInsurance(){
+        InsuranceCalculator insuranceCalculator = new InsuranceCalculator();
+        double insurance = insuranceCalculator.calculateInsurance(5000);
+        assertEquals(5000*0.365,insurance);
+    }
+
+    @Test
+    public void medInsurance(){
+        InsuranceCalculator insuranceCalculator = new InsuranceCalculator();
+        double insurance = insuranceCalculator.calculateInsurance(22000);
+        assertEquals((22000-10000)*0.2+35600,insurance);
+    }
+
+    @Test
+    public void highInsurance(){
+        InsuranceCalculator insuranceCalculator = new InsuranceCalculator();
+        double insurance = insuranceCalculator.calculateInsurance(52750);
+        assertEquals((52750-30000)*0.1+76500,insurance);
+    }
+
+    @Test
+    public void veryHighInsurance(){
+        InsuranceCalculator insuranceCalculator = new InsuranceCalculator();
+        double insurance = insuranceCalculator.calculateInsurance(75000);
+        assertEquals((75000-60000)*0.02+105600,insurance);
+
+    }
+
+
 
 }
